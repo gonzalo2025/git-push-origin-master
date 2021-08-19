c9f8f140fe68e18b54c10ace5c618c7827d2ea20
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <grupodigitalgrafico@gmail.com>
2021-08-19 01:37:51 -0400

    rails task manager ejercicio 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,38 @@
 class TasksController < ApplicationController
     def tasks
-       @tasks=Task.all 
+       @tasks = Task.all 
     end
 
     def create
+        @tasks = Task.new(params[:task])
+        @tasks.save
     end
+
+    def new
+        @task = Task.new
+    end
+
+    def edit
+        @task = Task.find(param[:id])
+    end
+
+    def show
+        @task = Task.find(param[:id])
+    end
+
+    def update
+        @task = Task.find(param[:id])
+    end
+
+    def delete
+        @task = Task.find(param[:id])
+    end
+
 end
+#PATCH  /restaurants/:id(.:format)      restaurants#update
+#DELETE /restaurants/:id(.:format)      restaurants#destroy
+#update
+#patch  '/tasks/:id', to: 'task#update'
+#delete
+#delete '/tasks/:id', to: 'task#destroy'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  #index
   get '/tasks', to: 'tasks#tasks'
+  #create
   post '/tasks', to: 'tasks#create'
+  #new
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  #edit
+  get '/tasks/:id/edit', to: 'tasks#edit',as: :edit_task
+  #show
+  get '/tasks/:id', to: 'tasks#show',as: :show_task
+  #update
+  patch  '/tasks/:id', to: 'tasks#update'
+  #delete
+  delete '/tasks/:id', to: 'tasks#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-
