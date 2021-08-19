1644f8f0d43d32b827ed8ace6c5be0c55f086429
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <grupodigitalgrafico@gmail.com>
2021-08-19 19:24:57 -0400

    ejercicio 6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
     def tasks
-       @tasks = Task.all 
+       @tasks = Task.all
     end
 
     def create
-        @tasks = Task.new(params[:task])
+        @tasks = Task.new(tasks_params)
         @tasks.save
     end
 
@@ -13,26 +13,28 @@ class TasksController < ApplicationController
     end
 
     def edit
-        @task = Task.find(param[:id])
+        @task = Task.find(params[:id])
     end
 
     def show
-        @task = Task.find(param[:id])
+        @task = Task.find(params[:id])
     end
 
     def update
-        @task = Task.find(param[:id])
+        @task = Task.find(params[:id])
+        @task.update(tasks_params)
+        redirect_to task_path(@task)
+
     end
+
 
     def delete
-        @task = Task.find(param[:id])
+        @task = Task.find(params[:id])
+        @tasks.destroy(tasks_params)
     end
-
+    private
+    def tasks_params
+        params.require(:task).permit(:title, :details)
+    end
 end
-#PATCH  /restaurants/:id(.:format)      restaurants#update
-#DELETE /restaurants/:id(.:format)      restaurants#destroy
-#update
-#patch  '/tasks/:id', to: 'task#update'
-#delete
-#delete '/tasks/:id', to: 'task#destroy'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   #index
   get '/tasks', to: 'tasks#tasks'
-  #create
-  post '/tasks', to: 'tasks#create'
   #new
   get '/tasks/new', to: 'tasks#new', as: :new_task
+  #create
+  post '/tasks', to: 'tasks#create'
   #edit
-  get '/tasks/:id/edit', to: 'tasks#edit',as: :edit_task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   #show
-  get '/tasks/:id', to: 'tasks#show',as: :show_task
+  get '/tasks/:id', to: 'tasks#show', as: :task
   #update
   patch  '/tasks/:id', to: 'tasks#update'
   #delete
   delete '/tasks/:id', to: 'tasks#destroy'
-
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
